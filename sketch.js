375ba67901c5cc5b3d14c33f2eea6d7a5e06d2ea
falakkumari <72171255+falakkumari@users.noreply.github.com>
2020-11-11 15:58:53 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var paper,box1,box2,box3
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	box1 = new Box (300,600,20,100)
+	box2 = new Box(500, 600, 20, 100)
+	box3 = new Box(400, 655, 200, 20)
+	paper = new Ball (100,100,20)
+	ground = new Ground(400,680,800,20)
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+	background(0);
+	box1.display();
+	box2.display();
+	box3.display();
+	paper.display();
+	ground.display();
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paper.body, paper.body.position, { x: 30, y: -30 })
+    }
+}
+
